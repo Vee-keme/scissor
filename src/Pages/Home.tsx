a6b3b60f6229bd239f6c4ae396777acb52515b02
Vee-keme <veliaprecious@gmail.com>
2023-12-04 14:21:54 +0100

    add images to relvolutionize section

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -7,6 +7,8 @@ import Pricing from "@/components/pricing/Pricing";
 import Analytics from "@/components/analytics/Analytics";
 import Faqs from "@/components/faqs/Faqs";
 import Footer from "@/components/footer/Footer";
+import leftImage from "@/assets/revolutionizing-left.png";
+import rightImage from "@/assets/revolutionizing-right.png";
 // type Props = {};
 
 const Home = () => {
@@ -33,7 +35,24 @@ const Home = () => {
           setSelectedPage={setSelectedPage}
         />
         <Faqs selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
-        <section className=" relative w-full h-full flex items-center justify-between flex-col bg-background-blue py-10 text-white">
+        <section className=" relative w-full h-full flex items-center justify-between flex-col gap-4 bg-background-blue py-10 text-white overflow-hidden">
+          <div>
+            {/* <img src={leftImage} alt="waves" className="h-3/4 w-3/4" /> */}
+            <img
+              src={leftImage}
+              alt="waves"
+              className=" absolute top-0 left-0  h-[90%]"
+            />
+          </div>
+
+          <div>
+            <img
+              src={rightImage}
+              alt="waves"
+              className=" absolute bottom-0 right-0 h-[90%]"
+            />
+          </div>
+
           <h2 className="text-4xl pb-6 font-semibold">
             Revolutionizing Link Optimization
           </h2>
